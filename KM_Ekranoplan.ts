cbc390c835135e00e5a629f732367da236434e50
FAQIHdimas2005 <123342985+FAQIHdimas2005@users.noreply.github.com>
2023-02-16 17:13:31 +0700

    Add files via upload

---
+++ KM_Ekranoplan.ts
@@ -0,0 +1,20 @@
+// soal no2
+
+const mt_25: string[] = [
+  "argodadi,sedayu",
+  "argomulyo,sedayu",
+  "argorejo,sedayu",
+  "argosari",
+  "bangunjiwo,kasihan",
+  "banguntapan,banguntapan",
+  "bantul,bantul",
+  "baturetno,banguntapan",
+  "bawuran,pleret",
+  "canden,jetis",
+  "caturharjo,pandak",
+  "cetan srigandi",
+];
+mt_25.sort((a: string, b: string) => a.localeCompare(b));
+console.log(mt_25);
+
+
